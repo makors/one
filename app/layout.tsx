88bf56621d4a8ca3fe4a65e0b5a4a42b077cb463
makors <makors@makors.xyz>
2026-03-07 12:36:06 -0500

    initial clerk installation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google"
-
+import { ClerkProvider } from "@clerk/nextjs"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'})
 
@@ -10,6 +11,11 @@ const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata: Metadata = {
+  title: "One",
+  description: "One is the omniscent platform for emergency dispatchers."
+}
 
 export default function RootLayout({
   children,
@@ -23,7 +29,9 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
     >
       <body>
+        <ClerkProvider>
         <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-import { Button } from "@/components/ui/button"
+import { RedirectToSignIn, Show } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+// placeholder page, redirects handles auth
+export default async function Page() {
+  const user = await currentUser();
+
+  if (user?.id) {
+    return redirect("/dashboard")
+  }
+
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <Show when="signed-out">
+      <RedirectToSignIn />
+    </Show>
   )
 }
